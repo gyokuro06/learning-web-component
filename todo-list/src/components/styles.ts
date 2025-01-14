4289bcafebaaee7fc15b90bf9c2947341a60126b
yuya.yagai <yuya.yagai@uzabase.com>
2025-01-14 21:45:08 +0900

    display mock todo data

---
+++ todo-list/src/components/styles.ts
@@ -0,0 +1,9 @@
+import { css } from "lit";
+
+export const todoListStyles = css`
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
--- todo-list/src/components/todo-list.ts
+++ todo-list/src/components/todo-list.ts
@@ -1,9 +1,41 @@
 import { html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
+import { todoListStyles } from "./styles";
+import { Task } from "@lit/task";
 
 @customElement('todo-list')
-export class TodoList extends LitElement {
-    render() {
-        return html`<div>Todo List</div>`
-    }
+class TodoList extends LitElement {
+  static styles = todoListStyles;
+
+  @property({ attribute: 'todo-list-id' })
+  todoListId = 'test-id';
+
+  private _fetchTodos = new Task(this, {
+    task: async ([todoListId], { signal }) => {
+      return {
+        todos: [
+          { id: 1, text: "Buy milk", status: "pending" },
+          { id: 2, text: "Walk the dog", status: "completed" },
+          { id: 3, text: "Do laundry", status: "pending" }
+        ]
+      }
+    },
+    args: () => [this.todoListId],
+  });
+
+  render() {
+    return html`
+      <div>
+        <h1>Todo List</h1>
+        ${this._fetchTodos.render({
+          pending: () => html`<div>Loading...</div>`,
+          complete: (todoItem) => html`
+            <ul>
+              ${todoItem.todos.map((todo) => html`
+                <li>${todo.text}</li>
+              `)}
+            </ul>`
+        })}
+      </div>`
+  }
 }
