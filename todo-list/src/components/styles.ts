916c38f3c22ba88fe86a4bd97e430a6e1dbfbac9
yuya.yagai <yuya.yagai@uzabase.com>
2025-02-04 09:04:51 +0900

    create list style

--- todo-list/src/components/styles.ts
+++ todo-list/src/components/styles.ts
@@ -7,3 +7,18 @@ export const todoListStyles = css`
     align-items: center;
   }
 `
+
+export const listTodoItemsStyles = css`
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: flex;
+    justify-content: space-between;
+    padding: 4px;
+  }
+  input[type="checkbox"] {
+    margin-right: 8px;
+  }
+`
